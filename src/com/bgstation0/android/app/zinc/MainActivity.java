353d9fcdac04a09aec8f865d8f56ea1a12b93157
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-16 11:43:08 +0900

    -メニューが選択された時の処理.

--- src/com/bgstation0/android/app/zinc/MainActivity.java
+++ src/com/bgstation0/android/app/zinc/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 //メインアクティビティクラスMainActivity
 public class MainActivity extends Activity {
@@ -27,6 +28,16 @@ public class MainActivity extends Activity {
     	// リソースからメニューを作成.
     	getMenuInflater().inflate(R.menu.menu_main, menu);	// getMenuInflater().inflateでR.menu.menu_mainからメニューを作成.
     	return true;	// trueを返す.
+    	
+    }
+    
+    // メニューが選択された時.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	
+    	// メニューアイテムのタイトルをトーストで表示.
+    	Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();	// item.getTitle()で取得したメニューアイテムのタイトルをToastで表示.
+    	return super.onOptionsItemSelected(item);	// 親クラスのonOptionsItemSelectedを呼ぶ.
     	
     }
     
