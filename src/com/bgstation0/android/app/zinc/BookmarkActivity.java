85f9bfbcdc6139e81840e21c0bef2418bd00c24b
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-16 17:36:05 +0900

    -リストビューの追加.

--- src/com/bgstation0/android/app/zinc/BookmarkActivity.java
+++ src/com/bgstation0/android/app/zinc/BookmarkActivity.java
@@ -2,8 +2,11 @@
 package com.bgstation0.android.app.zinc;
 
 //パッケージのインポート
+import java.util.ArrayList;
+import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 // ブックマークアクティビティクラスBookmarkActivity
 public class BookmarkActivity extends Activity {
@@ -15,6 +18,33 @@ public class BookmarkActivity extends Activity {
     	// ビューのセット
         super.onCreate(savedInstanceState);	// 親クラスのonCreateを呼ぶ.
         setContentView(R.layout.activity_bookmark);	// setContentViewでR.layout.activity_bookmarkをセット.
+
+        // bookmarkの作成
+        List<BookmarkItem> bookmark = new ArrayList<BookmarkItem>();	// ブックマークbookmarkの生成.
+
+        // adapterの生成
+        BookmarkAdapter adapter = new BookmarkAdapter(this, R.layout.adapter_bookmark_item, bookmark);	// アダプタadapterの生成.
+        
+        // ListViewの取得
+        ListView lvBookmark = (ListView)findViewById(R.id.listview_bookmark);	// リストビューlvBookmarkの取得.
+        
+        // リストビューにアダプタをセット.
+        lvBookmark.setAdapter(adapter);	//  lvBookmark.setAdapterでadapterをセット.
+        
+        // 仮アイテムの追加.
+        BookmarkItem item1 = new BookmarkItem();	// item1を生成.
+        item1.title = "test1";	// titleは"test1".
+        item1.url = "http://test1.com";	// urlは"http://test1.com"
+        BookmarkItem item2 = new BookmarkItem();	// item2を生成.
+        item2.title = "test2";	// titleは"test2".
+        item2.url = "http://test2.com";	// urlは"http://test2.com"
+        BookmarkItem item3 = new BookmarkItem();	// item3を生成.
+        item3.title = "test3";	// titleは"test3".
+        item3.url = "http://test3.com";	// urlは"http://test3.com"
+        adapter.add(item1);	// adapter.addでitem1を追加.
+        adapter.add(item2);	// adapter.addでitem2を追加.
+        adapter.add(item3);	// adapter.addでitem3を追加.
+        adapter.notifyDataSetChanged();	// adapter.notifyDataSetChangedで更新.
         
     }
     
---
+++ src/com/bgstation0/android/app/zinc/BookmarkAdapter.java
@@ -0,0 +1,45 @@
+// パッケージの名前空間
+package com.bgstation0.android.app.zinc;
+
+//パッケージのインポート
+import java.util.List;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+// ブックマークアダプタクラスBookmarkAdapter
+public class BookmarkAdapter extends ArrayAdapter<BookmarkItem> {
+
+	// メンバフィールドの定義
+	private LayoutInflater inflater;	// LayoutInflaterオブジェクトinflater
+	
+	// コンストラクタ
+	public BookmarkAdapter(Context context, int resource, List<BookmarkItem> objects){
+		
+		// インフレータの取得
+		super(context, resource, objects);	// 親コンストラクタを呼ぶ.
+		inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);	// context.getSystemServiceでLAYOUT_INFLATER_SERVICEを取得し, inflaterに格納.
+		
+	}
+	
+	// ビューの取得
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+		
+		// convertViewから取得したそれぞれのビューにアイテムの各要素をセットする.
+		if (convertView == null){	// convertViewがnullの時.
+			// インフレータがリソースから生成.
+			convertView = inflater.inflate(R.layout.adapter_bookmark_item, null);	// inflater.inflateでR.layout.adapter_bookmark_itemからconvertViewを生成.
+		}
+		TextView tvTitle = (TextView)convertView.findViewById(R.id.textview_bookmark_item_title);	// findViewByIdでR.id.textview_bookmark_item_titleからtvTitleを取得.
+		TextView tvUrl = (TextView)convertView.findViewById(R.id.textview_bookmark_item_url);	// findViewByIdでR.id.textview_bookmark_item_urlからtvUrlを取得.
+		tvTitle.setText(getItem(position).title);	// getItem(position)で取得したBookmarkItemのtitleをtvTitle.setTextでセット.
+		tvUrl.setText(getItem(position).url);	// getItem(position)で取得したBookmarkItemのurlをtvUrl.setTextでセット.
+		return convertView;	// convertViewを返す.
+		
+	}
+	
+}
