071f945555abcb39b429d2cbffdeb8886fbea37d
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-16 11:37:16 +0900

    -メニューの設置.

--- src/com/bgstation0/android/app/zinc/MainActivity.java
+++ src/com/bgstation0/android/app/zinc/MainActivity.java
@@ -1,15 +1,33 @@
+// パッケージの名前空間
 package com.bgstation0.android.app.zinc;
 
+//パッケージのインポート
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//メインアクティビティクラスMainActivity
 public class MainActivity extends Activity {
 
+	// アクティビティが作成された時.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    	
+    	// ビューのセット
+        super.onCreate(savedInstanceState);	// 親クラスのonCreateを呼ぶ.
+        setContentView(R.layout.activity_main);	// setContentViewでR.layout.activity_mainをセット.
+        
     }
+    
+    // メニューが作成された時.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	
+    	// リソースからメニューを作成.
+    	getMenuInflater().inflate(R.menu.menu_main, menu);	// getMenuInflater().inflateでR.menu.menu_mainからメニューを作成.
+    	return true;	// trueを返す.
+    	
+    }
+    
 }
