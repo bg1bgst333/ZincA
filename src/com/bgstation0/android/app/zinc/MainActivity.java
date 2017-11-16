6e52aafddae004349b904704159e89691ccd4ce8
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-16 12:16:26 +0900

    -ウェブビューの追加.

--- src/com/bgstation0/android/app/zinc/MainActivity.java
+++ src/com/bgstation0/android/app/zinc/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -60,20 +61,22 @@ public class MainActivity extends Activity implements OnClickListener {	// View.
     	// ボタンごとに振り分ける.
     	switch (v.getId()){	// v.getId()でView(Button)のidを取得.
     	
-    		// R.id.button_navigate("送信")の時.
-    		case R.id.button_navigate:
-    			
-    			// button_navigateブロック
-    			{
-    				
-    				// URLをトーストで表示.
-    	    		EditText etUrl = (EditText)findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
-    	    		Toast.makeText(this, etUrl.getText(), Toast.LENGTH_LONG).show();	// etUrl.getText()で取得したURLをToastで表示.
-
-    			}
-    			
-    			// 抜ける.
-    			break;	// breakで抜ける.
+	    	// R.id.button_navigate("送信")の時.
+			case R.id.button_navigate:
+				
+				// button_navigateブロック
+				{
+					
+					// ウェブビューで入力されたURLのWebページを表示.
+		    		EditText etUrl = (EditText)findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
+		    		WebView webView = (WebView)findViewById(R.id.webview);	// findViewByIdでR.id.webviewからWebViewオブジェクトwebViewを取得.
+		    		String url = etUrl.getText().toString();	// etUrl.getText().toString()で入力されたURL文字列を取得し, urlに格納.
+		    		webView.loadUrl(url);	// webView.loadUrlでurlの指すWebページをロード.
+	
+				}
+				
+				// 抜ける.
+				break;	// breakで抜ける.
     			
     		// それ以外の時.
     		default:
