859d6cf6d70a1645a648efdad2e99bdb1a8a992c
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-16 11:52:57 +0900

    -エディットテキストの追加.

--- src/com/bgstation0/android/app/zinc/MainActivity.java
+++ src/com/bgstation0/android/app/zinc/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 //メインアクティビティクラスMainActivity
@@ -35,8 +36,13 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
     	
-    	// メニューアイテムのタイトルをトーストで表示.
-    	Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();	// item.getTitle()で取得したメニューアイテムのタイトルをToastで表示.
+    	// 選択されたメニューアイテムごとに振り分ける.
+    	int id = item.getItemId();	// item.getItemIdで選択されたメニューアイテムのidを取得.
+    	if (id == R.id.menu_item_bookmark_add){	// R.id.menu_item_bookmark_add("ブックマークの追加")の時.
+    		// URLをトーストで表示.
+    		EditText etUrl = (EditText)findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
+    		Toast.makeText(this, etUrl.getText(), Toast.LENGTH_LONG).show();	// etUrl.getText()で取得したURLをToastで表示.
+    	}
     	return super.onOptionsItemSelected(item);	// 親クラスのonOptionsItemSelectedを呼ぶ.
     	
     }
