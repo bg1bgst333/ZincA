2d0036aa5c7b022def89c1ac69a69e220311dad0
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-17 13:42:15 +0900

    -履歴の登録.

--- src/com/bgstation0/android/app/zinc/CustomWebViewClient.java
+++ src/com/bgstation0/android/app/zinc/CustomWebViewClient.java
@@ -3,11 +3,14 @@ package com.bgstation0.android.app.zinc;
 
 //パッケージのインポート
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.provider.Browser;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.Toast;
 
 // カスタムウェブビュークライアントクラスCustomWebViewClient
 public class CustomWebViewClient extends WebViewClient {
@@ -31,9 +34,25 @@ public class CustomWebViewClient extends WebViewClient {
 			
 		// mContextからActivityを取得し,　そこから各Viewを取得.
 		if (mContext != null){	// mContextがnullでなければ.
+			
+			// URLバーに反映.
 			Activity activity = (Activity)mContext;	// mContextをActivityにキャストし, activityに格納.
 			EditText etUrl = (EditText)activity.findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
 			etUrl.setText(url);	// etUrl.SetTextでURLバーのetUrlにurlをセット.
+			
+			// このURLを履歴に登録.
+			Toast.makeText(activity, url, Toast.LENGTH_LONG).show();	// Toastでurlを表示.
+			ContentValues values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
+			values.put(Browser.BookmarkColumns.TITLE, view.getTitle());	// values.putでview.getTitleで取得したタイトルを登録.
+			values.put(Browser.BookmarkColumns.URL, url);	// values.putでurlを登録.
+			values.put(Browser.BookmarkColumns.BOOKMARK, "0");	// values.putでBOOKMARKフラグは"0"として登録.
+			try{	// tryで囲む.
+				activity.getContentResolver().insert(Browser.BOOKMARKS_URI, values);	// activity.getContentResolver().insertでvaluesを挿入.
+			}
+			catch (Exception ex){	// 例外のcatch.
+				Toast.makeText(activity, ex.getMessage(), Toast.LENGTH_LONG).show();	// ex.getMessageで取得した例外メッセージをToastで表示.
+			}
+			
 		}
 			
 	}
