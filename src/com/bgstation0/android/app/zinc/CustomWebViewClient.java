069bce680d360d41f35c48ed44970a7eda5d6f58
bg1bgst333 <bg1bgst333@gmail.com>
2017-11-21 12:21:01 +0900

    -CustomWebViewClientの整理.

--- src/com/bgstation0/android/app/zinc/CustomWebViewClient.java
+++ src/com/bgstation0/android/app/zinc/CustomWebViewClient.java
@@ -42,21 +42,13 @@ public class CustomWebViewClient extends WebViewClient {
 		// urlをログに出力.
 		Log.d(TAG, "onPageStarted: url = " + url);	// Log.dでurlを出力.
 		
-		// mContextからActivityを取得し,　そこから各Viewを取得.
-		if (mContext != null){	// mContextがnullでなければ.
+		// URLバーにURLをセット.
+		setUrl(url);	// setUrlでurlをセット.
 			
-			// URLバーに反映.
-			Activity activity = (Activity)mContext;	// mContextをActivityにキャストし, activityに格納.
-			//Toast.makeText(activity, "onPageStarted: url = " + url, Toast.LENGTH_LONG).show();	// Toastでurlを表示.
-			EditText etUrl = (EditText)activity.findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
-			etUrl.setText(url);	// etUrl.SetTextでURLバーのetUrlにurlをセット.
-		
-			// ロードを開始したURLを保持しておく.
-			mStartUrl = url;	// mStartUrlにurlをセット.
-			mCount = 0;	// mCountも0にしておく.
-			
-		}
-			
+		// ロードを開始したURLを保持しておく.
+		mStartUrl = url;	// mStartUrlにurlをセット.
+		mCount = 0;	// mCountも0にしておく.
+					
 	}
 		
 	// ロードするURLが変更された時.
@@ -66,13 +58,8 @@ public class CustomWebViewClient extends WebViewClient {
 		// urlをログに出力.
 		Log.d(TAG, "shouldOverrideUrlLoading: url = " + url);	// Log.dでurlを出力.
 				
-		// mContextからActivityを取得し,　そこから各Viewを取得.
-		if (mContext != null){	// mContextがnullでなければ.
-			Activity activity = (Activity)mContext;	// mContextをActivityにキャストし, activityに格納.
-			//Toast.makeText(activity, "shouldOverrideUrlLoading: url = " + url, Toast.LENGTH_LONG).show();	// Toastでurlを表示.
-			EditText etUrl = (EditText)activity.findViewById(R.id.edittext_urlbar);	// findViewByIdでR.id.edittext_urlbarからEditTextオブジェクトetUrlを取得.
-			etUrl.setText(url);	// etUrl.SetTextでURLバーのetUrlにurlをセット.
-		}
+		// URLバーにURLをセット.
+		setUrl(url);	// setUrlでurlをセット.
 				
 		// Chromeなど既定のブラウザで開かないようにするにはfalseを返す.
 		return false;	// falseを返す.
@@ -89,53 +76,60 @@ public class CustomWebViewClient extends WebViewClient {
 		// urlをログに出力.
 		Log.d(TAG, "onPageFinished: url = " + url);	// Log.dでurlを出力.
 			
-		// mContextからActivityを取得し, それのgetContentResolverを使う.
+		// 履歴登録条件を満たすかどうかを判定.
+		if (url.equals(mStartUrl) && mCount == 0){	// 直近の開始URLで一番最初の時.
+			addHistory(view, url);	// addHistoryでurlを履歴に登録.
+		}
+		mCount++;	// mCountを1増やす.
+				
+	}
+	
+	// URLバーにURLをセット.
+	public void setUrl(String url){
+		
+		// mContextからMainActivityを取得し, MainActivityのURLバーにセット.
 		if (mContext != null){	// mContextがnullでなければ.
-			
-			// 前回のURLと違う場合は履歴に登録.
-			Activity activity = (Activity)mContext;	// mContextをActivityにキャストし, activityに格納.
-			//if (!url.equals(mFinishUrl)){	// urlがmFinishUrlと違う場合.
-			if (url.equals(mStartUrl)){	// 直近の開始URLと同じ.
-				if (mCount == 0){	// しかも一番最初の時.
-
-					// このURLを履歴に登録.
-					//Toast.makeText(activity, "onPageFinished: url = " + url, Toast.LENGTH_LONG).show();	// Toastでurlを表示.
-					ContentValues values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
-					values.put(Browser.BookmarkColumns.TITLE, view.getTitle());	// values.putでview.getTitleで取得したタイトルを登録.
-					values.put(Browser.BookmarkColumns.URL, url);	// values.putでurlを登録.
-					values.put(Browser.BookmarkColumns.DATE, System.currentTimeMillis());	// 現在時刻を登録.
-					values.put(Browser.BookmarkColumns.BOOKMARK, "0");	// values.putでBOOKMARKフラグは"0"として登録.
-					try{	// tryで囲む.
-						Uri uri = activity.getContentResolver().insert(Browser.BOOKMARKS_URI, values);	// activity.getContentResolver().insertでvaluesを挿入.(Uriオブジェクトuriに格納.)
-						if (uri == null){	// 既に挿入されている場合, nullが返る模様.
-							values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
-							values.put(Browser.BookmarkColumns.DATE, System.currentTimeMillis());	// 現在時刻を登録.
-							int row = activity.getContentResolver().update(Browser.BOOKMARKS_URI, values, Browser.BookmarkColumns.URL + "=?", new String[]{url});	// activity.getContentResolver().updateでURLが同じ行を更新.
-							if (row < 0){
-								//Toast.makeText(activity, "error: "+url, Toast.LENGTH_LONG).show();
-								Toast.makeText(activity, activity.getString(R.string.toast_message_history_regist_error), Toast.LENGTH_LONG).show();	// R.string.toast_message_history_regist_errorに定義されたメッセージをToastで表示.
-							}
-							else{
-								//Toast.makeText(activity, "update: "+url, Toast.LENGTH_LONG).show();
-							}
-						}
-						else{
-							//Toast.makeText(activity, "insert: "+url, Toast.LENGTH_LONG).show();
-						}
-						//Toast.makeText(activity, "Uri="+uri, Toast.LENGTH_LONG).show();
-						//mFinishUrl = url;	// mFinishUrlにurlをセット.
-					}
-					catch (Exception ex){	// 例外のcatch.
-						//Toast.makeText(activity, ex.getMessage(), Toast.LENGTH_LONG).show();	// ex.getMessageで取得した例外メッセージをToastで表示.
-						Toast.makeText(activity, activity.getString(R.string.toast_message_history_regist_error), Toast.LENGTH_LONG).show();	// R.string.toast_message_history_regist_errorに定義されたメッセージをToastで表示.
-					}
 					
-				}
-			}
-			mCount++;	// mCountを1増やす.
+			// URLバーに反映.
+			MainActivity mainActivity = (MainActivity)mContext;	// mContextをMainActivityにキャストし, mainActivityに格納.
+			mainActivity.setUrl(url);	// mainActivity.setUrlでURLバーにURLをセット.
 			
 		}
 				
+	}
+	
+	// 履歴にURLを登録.
+	public void addHistory(WebView view, String url){
+		
+		// mContextからMainActivityを取得し, それのgetContentResolverを使う.
+		if (mContext != null){	// mContextがnullでなければ.
+			
+			// MainActivityにキャスト.
+			MainActivity mainActivity = (MainActivity)mContext;	// mContextをMainActivityにキャストし, mainActivityに格納.
+					
+			// このURLを履歴に登録.
+			ContentValues values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
+			values.put(Browser.BookmarkColumns.TITLE, view.getTitle());	// values.putでview.getTitleで取得したタイトルを登録.
+			values.put(Browser.BookmarkColumns.URL, url);	// values.putでurlを登録.
+			values.put(Browser.BookmarkColumns.DATE, System.currentTimeMillis());	// 現在時刻を登録.
+			values.put(Browser.BookmarkColumns.BOOKMARK, "0");	// values.putでBOOKMARKフラグは"0"として登録.
+			try{	// tryで囲む.
+				Uri uri = mainActivity.getContentResolver().insert(Browser.BOOKMARKS_URI, values);	// mainActivity.getContentResolver().insertでvaluesを挿入.(Uriオブジェクトuriに格納.)
+				if (uri == null){	// 既に挿入されている場合, nullが返る模様.
+					values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
+					values.put(Browser.BookmarkColumns.DATE, System.currentTimeMillis());	// 現在時刻を登録.
+					int row = mainActivity.getContentResolver().update(Browser.BOOKMARKS_URI, values, Browser.BookmarkColumns.URL + "=?", new String[]{url});	// mainActivity.getContentResolver().updateでURLが同じ行を更新.
+					if (row < 0){
+						Toast.makeText(mainActivity, mainActivity.getString(R.string.toast_message_history_regist_error), Toast.LENGTH_LONG).show();	// R.string.toast_message_history_regist_errorに定義されたメッセージをToastで表示.
+					}
+				}
+			}
+			catch (Exception ex){	// 例外のcatch.
+				Toast.makeText(mainActivity, mainActivity.getString(R.string.toast_message_history_regist_error), Toast.LENGTH_LONG).show();	// R.string.toast_message_history_regist_errorに定義されたメッセージをToastで表示.
+			}
+			
+		}
+		
 	}
 	
 }
