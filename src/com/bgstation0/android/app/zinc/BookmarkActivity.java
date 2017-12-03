3c84a3d4223abad70a8d2bc4ab976cb79fd665b9
bg1bgst333 <bg1bgst333@gmail.com>
2017-12-03 15:25:36 +0900

    -ブックマークの全削除の実装.

--- src/com/bgstation0/android/app/zinc/BookmarkActivity.java
+++ src/com/bgstation0/android/app/zinc/BookmarkActivity.java
@@ -14,6 +14,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Browser;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
@@ -26,6 +28,7 @@ public class BookmarkActivity extends Activity implements OnItemClickListener, O
 
 	// メンバフィールドの初期化.
 	public static final int DIALOG_ID_CONFIRM_BOOKMARK_REMOVE = 0;	// ブックマーク削除確認のダイアログID.
+	public static final int DIALOG_ID_CONFIRM_ALL_BOOKMARKS_REMOVE = 1;	// ブックマーク全削除確認のダイアログID.
 	
 	// アクティビティが作成された時.
     @Override
@@ -38,6 +41,34 @@ public class BookmarkActivity extends Activity implements OnItemClickListener, O
         // ブックマークのロード.
         loadBookmarks();	// loadBookmarksでブックマークをロードして表示.
         
+    }
+    
+    // メニューが作成された時.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	
+    	// リソースからメニューを作成.
+    	getMenuInflater().inflate(R.menu.menu_bookmark, menu);	// getMenuInflater().inflateでR.menu.menu_bookmarkからメニューを作成.
+    	return true;	// trueを返す.
+    	
+    }
+    
+    // メニューが選択された時.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	
+    	// 選択されたメニューアイテムごとに振り分ける.
+    	int id = item.getItemId();	// item.getItemIdで選択されたメニューアイテムのidを取得.
+    	if (id == R.id.menu_item_remove_all_bookmarks){	// R.id.menu_item_remove_all_bookmarks("全てのブックマークを削除")の時.
+    		
+    		// ブックマークの全削除.
+    		showConfirmAllBookmarksRemove();	// showConfirmAllBookmarksRemoveで全削除するか確認する.
+    		
+    	}
+    	
+    	// あとは既定の処理に任せる.
+    	return super.onOptionsItemSelected(item);	// 親クラスのonOptionsItemSelectedを呼ぶ.
+    	
     }
     
     // リストビューのアイテムが選択された時.
@@ -90,6 +121,36 @@ public class BookmarkActivity extends Activity implements OnItemClickListener, O
 			    	final ListView lv = (ListView)findViewById(R.id.listview_bookmark);	// リストビューlvの取得.
 			    	final BookmarkItem item = (BookmarkItem)lv.getItemAtPosition(position);	// lv.getItemAtPositionでitemを取得.
 			    	removeBookmark(item);	// removeBookmarkで削除.
+			    	removeDialog(id);	// removeDialogでダイアログを削除.
+				}				
+				
+			});
+    		Dialog dialog = builder.create();	// builder.createでdialogを作成.(ただし, まだ返さない.)
+    		dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {	// キャンセル時の動作を追加.
+
+    			// ダイアログのキャンセル時.
+				@Override
+				public void onCancel(DialogInterface dialog) {
+					removeDialog(id);	// removeDialogでダイアログを削除.
+				}
+				
+			});
+    		return dialog;	// dialogを返す.
+    		
+    	}
+    	else if (id == DIALOG_ID_CONFIRM_ALL_BOOKMARKS_REMOVE){	// ブックマーク全削除確認.
+    		
+    		// アラートダイアログの作成.
+    		AlertDialog.Builder builder = new AlertDialog.Builder(this);	// builderの作成.
+    		builder.setTitle(getString(R.string.dialog_title_confirm_all_bookmarks_remove));	// タイトルのセット.
+    		builder.setMessage(getString(R.string.dialog_message_confirm_all_bookmarks_remove));	// メッセージのセット.
+    		builder.setPositiveButton(getString(R.string.dialog_button_positive_yes), new DialogInterface.OnClickListener() {
+    			
+    			// "はい"ボタンが選択された時.
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// 全てのブックマークを削除.
+			    	removeAllBookmarks();	// removeAllBookmarksで全削除.
 			    	removeDialog(id);	// removeDialogでダイアログを削除.
 				}				
 				
@@ -206,6 +267,29 @@ public class BookmarkActivity extends Activity implements OnItemClickListener, O
         
     }
     
+    // ブックマークの全削除.
+    public void removeAllBookmarks(){
+    	
+    	// BOOKMARKフラグの立っている行全てのをBOOKMARKフラグを降ろす.
+    	ContentValues values = new ContentValues();	// ContentValuesオブジェクトvaluesの生成.
+    	values.put(Browser.BookmarkColumns.BOOKMARK, "0");	// values.putでBOOKMARKフラグは"0"として登録.
+    	int row = getContentResolver().update(Browser.BOOKMARKS_URI, values, Browser.BookmarkColumns.BOOKMARK + "=1", null);	// getContentResolver().updateでBOOKMARKが1の行を更新.
+    	//Toast.makeText(this, String.valueOf("delete row = " + row), Toast.LENGTH_LONG).show();	// rowをToastで表示.
+    	
+    	// ListViewの取得
+        ListView lvBookmark = (ListView)findViewById(R.id.listview_bookmark);	// リストビューlvBookmarkの取得.
+        
+        // adapterの取得.
+        BookmarkAdapter adapter = (BookmarkAdapter)lvBookmark.getAdapter();	// lvBookmark.getAdapterでadapterを取得.
+        
+        // 全削除.
+        adapter.clear();	// adapter.clearで全削除.
+        
+        // 更新.
+        adapter.notifyDataSetChanged();	// adapter.notifyDataSetChangedで更新.
+        
+    }
+    
     // ブックマークの削除確認ダイアログ.
     private void showConfirmBookmarkRemove(int position){
     
@@ -214,6 +298,14 @@ public class BookmarkActivity extends Activity implements OnItemClickListener, O
     	bundle.putInt("position", position);	// positionを登録.
     	showDialog(DIALOG_ID_CONFIRM_BOOKMARK_REMOVE, bundle);	// showDialogにDIALOG_ID_CONFIRM_BOOKMARK_REMOVEとbundleを渡す.
     	
-    }    
+    }
+    
+    // ブックマークの全削除確認ダイアログ.
+    private void showConfirmAllBookmarksRemove(){
+    	
+    	// ダイアログの表示.
+    	showDialog(DIALOG_ID_CONFIRM_ALL_BOOKMARKS_REMOVE);	// showDialogにDIALOG_ID_CONFIRM_ALL_BOOKMARKS_REMOVEを渡す.
+    	
+    }
     
 }
